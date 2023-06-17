6c58b6a83f3a320ab7a8d05bc27551552c9201d2
tensei <908739+tnsicdr@users.noreply.github.com>
2023-06-17 16:22:10 -0400

    chore: Initialize storybook (#1)

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storyboook/addon-styling",
+      options: {
+        postCss: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ src/lib/components/Button/Button.stories.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  render: () => <Button>Press me</Button>,
+};
