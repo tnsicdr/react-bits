26fc09f269679e734ef58e720f897669baf72e0f
tensei <908739+tnsicdr@users.noreply.github.com>
2024-05-23 12:34:29 -0400

    chore: fix test typing in vscode

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,10 @@
+/// <reference types="@testing-library/jest-dom" />
 import "@testing-library/jest-dom/vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+
+expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
