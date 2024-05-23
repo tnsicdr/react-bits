1cfe14984d956d849f32bf17d1f33af6d0f6dc60
tensei <908739+tnsicdr@users.noreply.github.com>
2024-05-23 12:34:17 -0400

    chore: remove placeholder button

--- src/lib/components/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Button from "./Button";
-
-describe("<Button />", () => {
-  it("should render children", () => {
-    render(<Button>Test Text</Button>);
-    expect(screen.queryByText("Test Text")).toBeTruthy();
-  });
-});
