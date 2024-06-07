4fe8567576d087a9b4f4a22776d4eb9722f1dc81
tensei <908739+tnsicdr@users.noreply.github.com>
2024-06-07 12:54:14 -0400

    fix: fix storybook paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@chromatic-com/storybook"
+    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/react-vite",
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
-import "./../src/styles/main.css";
+import "../lib/styles/main.css";
 
 const preview: Preview = {
   decorators: [
